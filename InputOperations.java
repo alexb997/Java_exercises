07155c50d13bd48eab64b201bca29d7324b66d82
alexb997 <alex_alexandru_997@yahoo.ro>
2021-11-01 11:22:49 +0200

    sixth exercise

---
+++ InputOperations.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class InputOperations {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input first number: ");
+        int x = in.nextInt();
+
+        System.out.print("Input second number: ");
+        int y = in.nextInt();
+
+        System.out.println(x + " + " + y + " = " + (x + y));
+
+        System.out.println(x + " - " + y + " = " + (x - y));
+
+        System.out.println(x + " x " + y + " = " + (x * y));
+
+        System.out.println(x + " / " + y + " = " + (x / y));
+
+        System.out.println(x + " mod " + y + " = " + (x % y));
+    }
+}
