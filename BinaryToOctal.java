4ff6c567a23af084748d883de340dbdf3e917396
alexb997 <alex_alexandru_997@yahoo.ro>
2021-11-02 10:58:43 +0200

    fixup! exercise 31'st

--- BinaryToOctal.java
+++ BinaryToOctal.java
@@ -4,14 +4,6 @@ import java.util.Scanner;
 
 public class BinaryToOctal {
     public static void main(String[] args) {
-        int binary_num;
-        Scanner in = new Scanner(System.in);
 
-        System.out.print("Input a Binary Number: ");
-        binary_num = Integer.parseInt(in.nextLine(), 2);
-
-        in.close();
-
-        System.out.println("Resulted octal number: " + Integer.toOctalString(binary_num));
     }
 }
